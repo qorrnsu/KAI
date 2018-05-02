2df2b8312cd60fb6797a1b41a350f48467ea513e
Woojung <qorrnsu@gmail.com>
2018-05-02 02:02:41 -0400

    activate gitignore

--- README.md
+++
@@ -1,5 +0,0 @@
-Korean artificial intelligence assistant with speech input and output.
-Run with python 3
-
-Dependencies:
-SpeechRecognition
--- tts.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-
-def speak(s): 
-    try:
-        tts = gTTS(text = s, lang = 'ko')
-        tts.save("output.mp3") 
-        return True
-    except:
-        return False 
